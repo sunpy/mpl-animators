135d4f15950b37cfe7521e08b1403cd6ec690028
Stuart Mumford <stuart@cadair.com>
2021-10-20 15:51:57 +0100

    Import code from sunpy

--- mpl_animators/__init__.py
+++ mpl_animators/__init__.py
@@ -1,4 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
 from .version import __version__
-__all__ = []
+
+from mpl_animators.base import *
+from mpl_animators.image import *
+from mpl_animators.line import *
+from mpl_animators.wcs import *
